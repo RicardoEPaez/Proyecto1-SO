236ef0bb27a00a91f7f1d19474eb351895fed5b3
RicardoEPaez <ricardo.paez@correo.unimet.edu.ve>
2026-01-21 18:53:31 -0400

    Estructura incial del proyecto

---
+++ Proyecto-SO/src/main/java/com/mycompany/proyecto/so/ProyectoSO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.so;
+
+/**
+ *
+ * @author ricar
+ */
+public class ProyectoSO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
