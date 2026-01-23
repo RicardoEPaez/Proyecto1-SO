ec3b2b27c639720d566fd6d89df6eafa0c248d3c
Home <Home@DESKTOP-MPNRUBV.lan>
2026-01-22 22:28:56 -0400

    Implementacion de las Estructuras de Datos a utilizar en el proyecto (Nodo, Nodo Prioridad, Cola, Cola Prioridad, Lista Enlazada)

---
+++ Proyecto-SO/src/main/java/estructuras/ListaEnlazada.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+/**
+ *
+ * @author Home
+ * @param <T>
+ */
+public class ListaEnlazada<T> {
+    public Nodo<T> inicio;
+    private int tamano;
+    
+    public ListaEnlazada(){
+        this.inicio = null;
+        this.tamano = 0;
+    }
+    
+    // Para insertar al final de la lista
+    public void agregar(T dato){
+        Nodo<T> nuevo = new Nodo <>(dato);
+        if (estaVacia()) {
+            inicio = nuevo;
+        } else {
+            Nodo<T> aux = inicio;
+            while (aux.getSiguiente() != null){
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevo);
+        }
+        tamano++;
+    }
+    
+    // Metodo para eliminar un objeto especifico, lo cual es util para mover procesos entre colas
+    public void eliminar(T dato) {
+        if (estaVacia()) return;
+        
+        if (inicio.getContenido().equals(dato)){
+            inicio = inicio.getSiguiente();
+            tamano--;
+        }
+        
+        Nodo<T> actual = inicio;
+        while (actual.getSiguiente() != null && !actual.getSiguiente().getContenido().equals(dato)){
+            actual = actual.getSiguiente();
+        }
+        
+        if (actual.getSiguiente() != null){
+            actual.setSiguiente(actual.getSiguiente().getSiguiente());
+            tamano--;
+        }
+    }
+    
+    // Obtener elementos por indice para recorrer la lista en bucles
+    public T get(int indice){
+        if (indice < 0 || indice >= tamano) return null;
+        
+        Nodo<T> actual = inicio;
+        for (int i = 0; i < indice; i++){
+            actual = actual.getSiguiente();
+        }
+        return actual.getContenido();
+    }
+    
+    // Para determinar si la lista esta vacia
+    public boolean estaVacia(){
+        return inicio == null;
+    }
+    
+    public int getTamano(){
+        return tamano;
+    }
+    
+    // Getter del nodo inicio, el cual es util si se necesita iterar manualmente fuera de la clase
+    public Nodo<T> getInicio(){
+        return inicio;
+    }
+}
